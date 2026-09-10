ab24fada491ad01a7628ef00ec15e28c54bed65c
Josephine Peters <josephine.peters@ukaea.uk>
2026-09-10 15:10:06 +0100

    fix typo

--- examples/spherical_tokamak/bluemira-spherical-tokamak.py
+++ examples/spherical_tokamak/bluemira-spherical-tokamak.py
@@ -72,8 +72,7 @@ def _():
     )
     os.environ.setdefault(key="BLUEMIRA_GEOMETRY_BACKEND", value="cadquery")
 
-    subprocess.run(["apt-get", "update", "-q"], check=True, ,
-    stdout=subprocess.DEVNULL)
+    subprocess.run(["apt-get", "update", "-q"], check=True, stdout=subprocess.DEVNULL)
 
     subprocess.run(
         ["apt-get", "install", "-y", "-q", "libglu1-mesa", "libgl1"],
