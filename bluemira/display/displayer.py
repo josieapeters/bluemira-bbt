fb5a10d652d8b8bc16618f2b843429ceff783379
Josephine Peters <josephine.peters@ukaea.uk>
2026-09-10 14:39:42 +0100

    trying out _shape instead of part.shape

--- bluemira/display/displayer.py
+++ bluemira/display/displayer.py
@@ -225,7 +225,7 @@ def show_cad(
             )
 
     backend.get_module().show_cad(
-        [part.shape for part in parts],
+        [part._shape for part in parts],
         [o.as_dict() for o in new_options],
         labels,
         **viewer_kwargs,
